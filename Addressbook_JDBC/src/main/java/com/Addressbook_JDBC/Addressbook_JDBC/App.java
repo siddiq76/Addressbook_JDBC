857bf4f0363147a1e08080d4228041ae9d4bffe2
“siddiq76” <siddiq.khan0786@gmail.com>
2021-03-18 17:19:33 +0530

    add

---
+++ Addressbook_JDBC/src/main/java/com/Addressbook_JDBC/Addressbook_JDBC/App.java
@@ -0,0 +1,13 @@
+package com.Addressbook_JDBC.Addressbook_JDBC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
